941c3f65156f14ff21ee74c3d26a5aa6cf98cff2
davidtorres1996 <46732138+davidtorres1996@users.noreply.github.com>
2023-11-19 14:29:51 -0500

    modificacion en el bot

--- flows/FlowFile.js
+++ flows/FlowFile.js
@@ -15,33 +15,15 @@ export const FlowParticipar = bot .addKeyword("Hola, quiero participar en el Tor
 
     if (Data !== null) {
 
-            await typing(provider, ctx, 2000);
             await flowDynamic([{
                 body: `${Data[0]}`,
                 media: 'https://atubot.net.pe/wp-content/uploads/2023/11/miguelon.jpeg'
                }]);
-            await typing(provider, ctx, 2000);
-            await provider.vendor.sendMessage(id, {
-                text: `${Data[1]}`,
-                contextInfo: {
-                  externalAdReply: {
-                    title: 'BIGO LIVE-Live Stream, Go Live',
-                    body: 'Click Aquí ☝️',
-                    mediaType: 'IMAGE', //VIDEO - IMAGE - NONE
-                    showAdAttribution: false, //Mensaje a partir de un anuncio
-                    //renderLargerThumbnail: true, 
-                    mediaUrl: 'https://bigolive.onelink.me/sG8X?af_web_dp=&af_dp=&is_retargeting=true&pid=region_kol&c=PE_miguelon',
-                    thumbnailUrl: 'https://atubot.net.pe/wp-content/uploads/2023/11/839.webp', //url imagen
-                    sourceUrl: 'https://bigolive.onelink.me/sG8X?af_web_dp=&af_dp=&is_retargeting=true&pid=region_kol&c=PE_miguelon',
-                  },
-                },
-              }); 
-            await typing(provider, ctx, 2000);
+            await flowDynamic(Data[1]);
             await flowDynamic([{
                 body: `${Data[2]}`,
                 media: 'https://atubot.net.pe/wp-content/uploads/2023/11/captura_miguelon.jpeg'
                }]);
-            await typing(provider, ctx, 2000);
             await flowDynamic(Data[3]);
 
 
